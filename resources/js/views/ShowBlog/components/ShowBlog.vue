6cc79e03292cd268c5907eabb25a63f1296db0ba
Dewi van der Velden <Dewi@intermix.nl>
2023-02-22 09:21:43 +0100

    Created the styling for a blog post

--- resources/js/views/ShowBlog/components/ShowBlog.vue
+++ resources/js/views/ShowBlog/components/ShowBlog.vue
@@ -3,23 +3,29 @@
         <img class="blog__header__image--profilePicture" :src="ProfilePicture">
         <span class="blog__header__text blog__header__text--username">@Gebruikersnaam</span>
         <span class="blog__header__text blog__header__text--timestamp">25 minuten geleden</span>
-        <span><img  class="blog__header__image blog__header__image--edit" :src="defaultEditIcon"></span>
+        <span><img class="blog__header__image blog__header__image--edit" :src="defaultEditIcon"></span>
         <span ><img class="blog__header__image blog__header__image--delete" :src="defaultDeleteIcon"></span>
     </div>
 
     <div class="blog__content">
-        <img class="blog__content--image" :src="ProfilePicture">
-        <p class="blog__content--title">Dit is de titel van een blog</p>
-        <textarea class="blog__content--description"></textarea>
-        <button class="blog__content--button">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium elit rhoncus nulla pulvinar lacinia.</button>
+        <img class="blog__content blog__content--image" :src="BlogPicture">
+        <p class="blog__content blog__content--title">Dit is de titel van een blog</p>
+        <p class="blog__content blog__content--description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pretium elit rhoncus nulla pulvinar lacinia.</p>
+        <button class="blog__content blog__content--button">Lees verder</button>
+    </div>
+
+    <div class="blog__comments">
+        <img class="blog__comments blog__comments--profilePicture" :src="ProfilePicture">
+        <input class="blog__comments blog__comments--text" type="text" placeholder="Schrijf een opmerking...">
     </div>
 </template>
 
 
 <script setup>
     import ProfilePicture from '../../../../assets/tyler-nix-PQeoQdkU9jQ-unsplash.jpg'
-    import defaultEditIcon from '../../../../assets/bin.png'
-    import defaultDeleteIcon from '../../../../assets/draw.png'
+    import BlogPicture from '../../../..//assets/lukas-blazek-GnvurwJsKaY-unsplash.jpg'
+    import defaultEditIcon from '../../../../assets/draw.png'
+    import defaultDeleteIcon from '../../../../assets/bin.png'
 </script>
 
 <script>
@@ -28,7 +34,7 @@ export default {
 
     data(){
         return {
-
+            
         }
     },
 
