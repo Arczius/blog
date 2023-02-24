fb7b3b456619761512f66c7a4df0aa228b88d618
Arczius <damianvaartmans@gmail.com>
2023-02-24 15:29:39 +0100

    added more functionality to the Tyler page

--- resources/js/views/Tyler/Index.vue
+++ resources/js/views/Tyler/Index.vue
@@ -1,6 +1,10 @@
 <template>
     <div :style="'--tyler-image: url(' + Tyler + ');'" class="tyler">
 
+        <div v-for="item in count">
+            <img @click="count++" :src="Tyler" :style="'--item-width: ' + item + ';'" >
+        </div>
+
     </div>
 </template>
 
@@ -12,6 +16,16 @@
     background-position: center;
     height: 100vh;
     width: 100%;
+    display: flex;
+    max-width: 100%;
+    flex-wrap: wrap;
+}
+
+.tyler div img {
+    width: calc(var(--item-width) * 16px);
+    height: calc(var(--item-width) * 16px);
+    object-fit: cover;
+    object-position: center;
 }
 </style>
 
@@ -21,6 +35,11 @@ import Tyler from "../../../assets/tyler-nix-PQeoQdkU9jQ-unsplash.jpg"
 
 <script>
 export default {
-    name: "Tyler"
+    name: "Tyler",
+    data(){
+        return {
+            'count': 1,
+        }
+    }
 }
 </script>
