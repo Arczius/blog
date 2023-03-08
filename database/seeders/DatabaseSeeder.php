7ef73343126a40cc3819a1c63a143b1e2bf5e16d
Arczius <damianvaartmans@gmail.com>
2023-03-08 09:53:25 +0100

    built migrations and seeders for the followers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             CategoriesPostsSeeder::class,
             PostSeeder::class,
             CommentSeeder::class,
+            FollowingSeeder::class,
         ]);
     }
 }
